404a144ec24f6c3da9d125ba6a0dc9b80613d1df
user <carola1408@yahoo.com.tw>
2023-01-11 21:44:01 +0800

    feat: add restaurant seeder

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 //定義要使用連接埠號 (port number)  define server related variables
 const port = 3000
+
+
+
 
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
+
 })
 
 //啟動並監聽伺服器 Listen the server when it started
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const restaurant = require('../restaurant') // 載入 restaurant model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 8; i++) {
+    restaurant.create({ name: `name-${i}` })
+  }
+  console.log('done')
+})
