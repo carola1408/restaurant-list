c00369a146ac0759454b02e0bb62955275f0be56
user <carola1408@yahoo.com.tw>
2023-01-14 23:40:14 +0800

    feat: add restaurant.json

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ db.once('open', () => {
 const port = 3000
 // require express-handlebars here 設定在 Express 中使用的樣版引擎
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json') // 載入 JSON
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -35,69 +36,8 @@ app.use(express.static('public'))
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  const restaurantList = [
-    {
-      id: 1,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg',
-      title: 'Sababa 沙巴巴中東美食',
-      category: ' 中東料理',
-      rating: 4.1
-
-    },
-    {
-      id: 2,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5628/02.jpg',
-      title: '梅子鰻蒲燒專賣店',
-      category: '日本料理',
-      rating: 4.3
-
-    },
-    {
-      id: 3,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5629/03.jpg',
-      title: 'ZIGA ZIGA',
-      category: '義式餐廳',
-      rating: 4.2
-
-    }, {
-      id: 4,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5630/04.jpg',
-      title: '艾朋牛排餐酒館',
-      category: '美式',
-      rating: 4.2
-
-    }, {
-      id: 5,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5631/05.jpg',
-      title: 'Gusto Pizza',
-      category: '義式餐廳',
-      rating: 4.7
-
-    }, {
-      id: 6,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5632/06.jpg',
-      title: 'WXYZ Bar店',
-      category: '酒吧',
-      rating: 4.3
-
-    }, {
-      id: 7,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5633/07.jpg',
-      title: 'Fika Fika Cafe',
-      category: '咖啡',
-      rating: 4.3
-
-    }, {
-      id: 8,
-      image: 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5634/08.jpg',
-      title: '布娜飛比利時啤酒餐廳',
-      category: '義式餐廳',
-      rating: 4.7
-
-    },
-  ]
-
-  res.render('index', { restaurants: restaurantList })
+  // pass the movie data into 'index' partial template
+  res.render('index', { restaurants: restaurantList.results })
 
 })
 
