d32cd5698820acaaf6d2a7faede98a275eafc98f
user <carola1408@yahoo.com.tw>
2023-01-15 23:15:09 +0800

    feat: modify restaurant seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const restaurant = require('../restaurant') // 載入 restaurant model
-const restaurant = require('../../restaurant.json') // 載入 JSON
+const restaurantList = require('../../restaurant.json') // 載入 JSON
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -11,8 +11,8 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < 8; i++) {
-    restaurant.create({ name: `name-${i}` })
+  for (let i = 0; i < restaurantList.length; i++) {
+    restaurant.create({ restaurantList })
   }
   console.log('done')
 })
