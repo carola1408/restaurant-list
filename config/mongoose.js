d1ba283c22fffa11ee66eb692b0e468579aa7de0
user <carola1408@yahoo.com.tw>
2023-02-16 16:12:03 +0800

    修改 mongoose連線

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose') // 載入 mongoose
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,23 +1,10 @@
-// const mongoose = require('mongoose') // 載入mongoose
-const db = require('../../config/mongoose')
 const restaurant = require('../restaurant') //載入restaurant model
+const db = require('../../config/mongoose')
+
 const restaurantList = require('../../restaurant.json').results// 載入 JSON
 
-// 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
-// // 設定連線到 mongoDB
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// // 取得資料庫的狀態
-// const db = mongoose.connection
-
-// // 檢查資料快連線狀態，連線異常
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
 // 檢查資料快連線狀態，連線正常
 //作法一
 db.once('open', () => {
