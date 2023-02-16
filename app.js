a10bf93ef8bd04dd1dbc637d96e1df8a59c63f57
user <carola1408@yahoo.com.tw>
2023-02-16 15:55:31 +0800

    refactor: modify mongoose config 重構mongoose連線

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
-const app = express()
 const port = 3000 // 定義要使用連接埠號(port number) 
 const exphbs = require('express-handlebars') // require express-handlebars 
 const restaurantList = require('./restaurant.json') // 載入 JSON
@@ -14,19 +12,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+require('./config/mongoose')
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+const app = express()
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -41,13 +29,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
-
-
-
-
-
-
 
 
 //啟動並監聽伺服器 Listen the server when it started
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose') // 載入mongoose
+// const mongoose = require('mongoose') // 載入mongoose
+const db = require('../../config/mongoose')
 const restaurant = require('../restaurant') //載入restaurant model
 const restaurantList = require('../../restaurant.json').results// 載入 JSON
 
@@ -7,16 +8,16 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// // 設定連線到 mongoDB
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 取得資料庫的狀態
-const db = mongoose.connection
+// // 取得資料庫的狀態
+// const db = mongoose.connection
 
-// 檢查資料快連線狀態，連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+// // 檢查資料快連線狀態，連線異常
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
 // 檢查資料快連線狀態，連線正常
 //作法一
 db.once('open', () => {
