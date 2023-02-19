b06743de3cec0ccfc449613f8ba99d528b778614
user <carola1408@yahoo.com.tw>
2023-02-19 10:20:33 +0800

    feat: add sort function 新增排序功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,11 +4,12 @@ const router = express.Router() // 準備引入路由模組
 
 const restaurant = require('../../models/restaurant') // 引用 restaurant model
 
-// 設定首頁home路由模組
+// 設定首頁home & sort路由模組
 router.get('/', (req, res) => {
   // pass the restaurant data into 'index' partial template
   restaurant.find()  // 取出 restaurant model 裡的所有資料
     .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc', category: 'desc', location: 'asc' }) // 新增這裡：排序
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.log(error)) //錯誤處理
 
@@ -17,6 +18,7 @@ router.get('/', (req, res) => {
 //設定search路由
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
+
   restaurant.find()
     .lean()
     .then(restaurants => {
@@ -28,7 +30,9 @@ router.get('/search', (req, res) => {
         }
       })
     })
-    .then((restaurants, keyword) => res.render('index', { restaurants, keyword }))
+    .then((restaurants, keyword,) => res.render('index', { restaurants, keyword, }))
 })
+
+
 // 匯出路由器
 module.exports = router
