aef7a647a4ab1878a20058df52ba17a483e45667
user <carola1408@yahoo.com.tw>
2023-03-19 22:39:56 +0800

    feat: add user authentication in restaurant and home routers 驗證登入狀態

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const router = express.Router() // 準備引入路由模組
 const home = require('./modules/home') // 引入 home 模組程式碼
 const restaurants = require('./modules/restaurants') // 引入 restaurants 模組程式碼
 const users = require('./modules/users') //引用 users 模組
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants) // 加入驗證程序
 // 將網址結構符合 /users 字串開頭的 request 導向 users 模組
 router.use('/users', users)
+// 將網址結構符合 / 字串的 request 導向 home 模組
+router.use('/', authenticator, home) // 加入驗證程序
+
 module.exports = router // 匯出路由模組
