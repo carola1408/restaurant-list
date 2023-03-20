8f134ae628a041e3b97f8f4491837c9924e33ce8
user <carola1408@yahoo.com.tw>
2023-03-20 09:42:53 +0800

    feat: modify mongoose and home router

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,3 +1,4 @@
+const { Types } = require('mongoose')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
@@ -35,7 +36,13 @@ const restaurantSchema = new Schema({
   description: {
     type: String,
   },
-
-
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
 })
+
+
 module.exports = mongoose.model('restaurant', restaurantSchema)
