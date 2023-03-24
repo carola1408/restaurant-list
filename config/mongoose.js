c20b1bd50b83f70586e9fc47e1cf076afcb6b0bd
user <carola1408@yahoo.com.tw>
2023-03-24 09:42:10 +0800

    feat: modify dotenv

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose') // 載入 mongoose
+const MONGODB_URI = process.env.MONGODB_URI
 
-// 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
