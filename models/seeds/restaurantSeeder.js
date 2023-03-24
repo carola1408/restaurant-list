b4a5bd91dc140eab81e7679d805d83a5482c659c
user <carola1408@yahoo.com.tw>
2023-03-24 10:25:03 +0800

    feat: add user.json

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,38 @@
+const bcrypt = require('bcryptjs') //引用 bcrypt
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const restaurant = require('../restaurant') //載入restaurant model
+const User = require('../user') // 準備引入User model模組
 const db = require('../../config/mongoose')
-
 const restaurantList = require('../../restaurant.json').results// 載入 JSON
 
 
 
 // 檢查資料快連線狀態，連線正常
-//作法一
+//重寫新的種子資料
+
 db.once('open', () => {
-  console.log("restaurantSeeder done!")
-  restaurant.create(restaurantList)
-    .catch(err => {
-      console.trace(err)
-    })
-    .finally(() => {
-      db.close()
-    })
+
 })
+
+
+
+
+
+
+
+//作法一
+// db.once('open', () => {
+//   console.log("restaurantSeeder done!")
+//   restaurant.create(restaurantList)
+//     .catch(err => {
+//       console.trace(err)
+//     })
+//     .finally(() => {
+//       db.close()
+//     })
+// })
 
 //作法二
 // db.once('open', () => {
