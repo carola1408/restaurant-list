333242e7249bca40840cfc4b84511e617793a2a7
user <carola1408@yahoo.com.tw>
2023-05-02 11:59:42 +0800

    modify facebook login

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose') // 載入 mongoose
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
+const MONGODB_URI = process.env.MONGODB_URI
 // 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
